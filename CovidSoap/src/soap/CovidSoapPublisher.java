4c052ebeb5a80d44f54aeee209ddd3e43fdfd3bd
magantus@googlemail.com <Extrem93!>
2021-02-14 18:47:31 +0100

    d

--- CovidSoap/src/soap/CovidSoapPublisher.java
+++ CovidSoap/src/soap/CovidSoapPublisher.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 public class CovidSoapPublisher {
 
 	public static void main(String[] args) {
-		Endpoint.publish("http://localhost:9999/covid/info", new CovidSoapImpl());
+		Endpoint.publish("https://covidsoap.herokuapp.com:9999/covid/info", new CovidSoapImpl());
 	}
 
 }
