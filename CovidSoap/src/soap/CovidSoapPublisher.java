1583960b5fa78843b2d6f97708184c456a1d4903
magantus@googlemail.com <Extrem93!>
2021-02-14 19:36:31 +0100

    d

--- CovidSoap/src/soap/CovidSoapPublisher.java
+++ CovidSoap/src/soap/CovidSoapPublisher.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 public class CovidSoapPublisher {
 
 	public static void main(String[] args) {
-		Endpoint.publish("http://localhost:9999/covid", new CovidSoapImpl());
+		Endpoint.publish("http://localhost:44255/covid", new CovidSoapImpl());
 	}
 
 }
