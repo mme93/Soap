106664ba0be135fa663bf3b30aaae6793aae6bc6
magantus@googlemail.com <Extrem93!>
2021-02-14 18:33:18 +0100

    finish

--- CovidSoap/src/soap/CovidSoapImpl.java
+++ CovidSoap/src/soap/CovidSoapImpl.java
@@ -2,23 +2,56 @@ package soap;
 
 import javax.jws.WebService;
 
+import org.json.JSONObject;
+
+import covidInfo.CalculateCovidNumber;
+import covidInfo.JHU;
+import covidInfo.RKI;
 
 //Service Implementation
 @WebService(endpointInterface = "soap.CovidSoapInfo")
 public class CovidSoapImpl implements CovidSoapInfo {
+	CalculateCovidNumber calculateCovidNumber;
 
-	@Override
-	public String getHelloWorldAsString(String name) {
-		
-			
-		return "Hello World JAX-WS " + name;
+	public CovidSoapImpl() {
+		super();
+		calculateCovidNumber = new CalculateCovidNumber(new RKI().getRKICovidInfo(), new JHU().getJHUCovidInfo());
 	}
 
 	@Override
-	public String getTotalInfection() {
-		
-		return "Ihr werdet alle sterben";
+	public String getNewInfectionsLastDayJHU() {
+		return new JSONObject().put("Wert", calculateCovidNumber.getRWerthTotalGermanyRKI()).toString();
+	}
+
+	@Override
+	public String getTotalInfectionsJHU(int rWerth) {
+		return new JSONObject().put("Wert", calculateCovidNumber.getTotalTargetInfectionRKI(rWerth)).toString();
+	}
+
+	@Override
+	public String getIncreaseLasteDayJHU() {
+		return new JSONObject().put("Wert", calculateCovidNumber.getTargetIncidenceForRWerthRKI(50,
+				calculateCovidNumber.getTotalTargetInfectionRKI(50), 7)).toString();
+	}
+
+	@Override
+	public String getAverageIncreaseDayJHU(int days) {
+		return new JSONObject().put("Wert", calculateCovidNumber.getAverageIncreaseDayJHU(days)).toString();
+	}
+
+	@Override
+	public String getRWerthTotalGermanyRKI() {
+		return new JSONObject().put("Wert", calculateCovidNumber.getIncreaseLasteDayJHU()).toString();
+	}
+
+	@Override
+	public String getTotalTargetInfectionRKI(double rZiel) {
+		return new JSONObject().put("Wert", calculateCovidNumber.getTotalInfectionsJHU()).toString();
+	}
+
+	@Override
+	public String getTargetIncidenceForRWerthRKI(double rZiel, double totalTargetInfection, int nDay) {
+		return new JSONObject().put("Wert", calculateCovidNumber.getNewInfectionsLastDayJHU()).toString();
 	}
 
 }
-
--- CovidSoap/src/soap/CovidSoapInfo.java
+++ CovidSoap/src/soap/CovidSoapInfo.java
@@ -1,9 +1,12 @@
 package soap;
 
+
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
+
 
 //Service Endpoint Interface
 @WebService
@@ -11,10 +14,34 @@ import javax.jws.soap.SOAPBinding.Style;
 public interface CovidSoapInfo {
 
 	@WebMethod
-	String getHelloWorldAsString(String name);
-	
-	@WebMethod String getTotalInfection();
-		
-	
+	public String getNewInfectionsLastDayJHU();
+
+	@WebMethod
+	public String getTotalInfectionsJHU(int rWerth);
+
+	@WebMethod
+	public String getIncreaseLasteDayJHU();
+
+	@WebMethod
+	public String getAverageIncreaseDayJHU(int days);
+
+	@WebMethod
+	private String getNewInfectionAtThisDay(int dayIndex) {
+		return null;
+	}
+
+	@WebMethod
+	public String getRWerthTotalGermanyRKI();
+
+	@WebMethod
+	public String getTotalTargetInfectionRKI(double rZiel);
+
+	@WebMethod
+	public String getTargetIncidenceForRWerthRKI(double rZiel, double totalTargetInfection, int nDay);
+
+	@WebMethod
+	private String getNDay(int nDay) {
+		return null;
+	}
 
 }
